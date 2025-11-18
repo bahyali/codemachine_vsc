e1c5f2b3b478dfb0bcc34accbae19572e9960f52
bahyali <bahy.ali@icloud.com>
2025-11-18 21:19:06 +0100

    Built Step I1.T1

--- project/src/extension.ts
+++ project/src/extension.ts
@@ -1,13 +1,14 @@
-import * as vscode from 'vscode'
+import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('The "Code Machine Orchestrator" extension is now active.')
 
-	const outputChannel = vscode.window.createOutputChannel('Code Machine')
-	outputChannel.appendLine('Code Machine Orchestrator activated.')
-	outputChannel.show(true) // Passing true preserves focus on the editor
+	console.log('Congratulations, your extension "code-machine-orchestrator" is now active!');
 
-	context.subscriptions.push(outputChannel)
+	const outputChannel = vscode.window.createOutputChannel('Code Machine');
+	outputChannel.appendLine('Code Machine Orchestrator activated.');
+	outputChannel.show(true);
+
+	context.subscriptions.push(outputChannel);
 }
 
 export function deactivate() {}
