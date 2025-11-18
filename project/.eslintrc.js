616211fb93a7abc1576f0563f3d8079e5bf97dbf
bahyali <bahy.ali@icloud.com>
2025-11-18 22:34:48 +0100

    Update todo list for codemachine_vscode_ext_v2: Req. Project

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-    "root": true,
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2020,
-        "sourceType": "module"
-    },
-    "env": {
-        "node": true,
-        "es6": true
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "rules": {
-        "semi": ["error", "never"],
-        "quotes": ["error", "single", { "avoidEscape": true }]
-    }
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		sourceType: 'module',
+	},
+	env: {
+		node: true,
+		es6: true,
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		'semi': ['error', 'always'],
+        '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
 };
--- project/src/extension.ts
+++ project/src/extension.ts
@@ -1,12 +1,14 @@
 import * as vscode from 'vscode';
+import { WorkflowController, Phase } from './controllers/WorkflowController';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('Congratulations, your extension "code-machine-orchestrator" is now active!');
-
 	const outputChannel = vscode.window.createOutputChannel('Code Machine');
+	outputChannel.appendLine('Congratulations, your extension "code-machine-orchestrator" is now active!');
 	outputChannel.appendLine('Code Machine Orchestrator activated.');
 	outputChannel.show(true);
+
+	const workflowController = new WorkflowController();
+	outputChannel.appendLine(`Workflow initialized in phase: ${Phase[workflowController.currentPhase]}`);
 
 	context.subscriptions.push(outputChannel);
 }
